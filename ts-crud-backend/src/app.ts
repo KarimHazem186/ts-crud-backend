c47084fe519ca32b4f114db1de90484eab22d9cf
KarimHazem186 <karimhazem186@gmail.com>
2025-08-02 22:34:08 +0300

    Update app.ts

--- ts-crud-backend/src/app.ts
+++ ts-crud-backend/src/app.ts
@@ -14,20 +14,7 @@ app.use('/api/users', userRoutes);
 // 404 handler
 app.use((_req, _res, next) => next(createHttpError(404, 'Not found')));
 
-// // 404 middleware
-// app.use((_req: Request, _res: Response, next: NextFunction) => {
-//   const error = new Error('Not Found');
-//   (error as any).status = 404;
-//   next(error);
-// });
-
-
 // Global error handler
-// app.use((err: unknown, req: Request, res: Response, _next: NextFunction) => {
-//   const { status = 500, message = 'Unknown error' } = err as any;
-//   res.status(status).json({ error: message });
-// });
-
 app.use((err: any, _req: Request, res: Response, _next: NextFunction) => {
   const status = err.status || 500;
   const message = err.message || 'Something went wrong';
