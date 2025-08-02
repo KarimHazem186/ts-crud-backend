0f9227e1d9d4f14151eec9340e74d1853903ac03
KarimHazem186 <karimhazem186@gmail.com>
2025-08-02 16:37:20 +0300

    Edit folder config & edit some configuration

--- ts-crud-backend/src/config/db.ts
+++ ts-crud-backend/src/config/db.ts
@@ -1,19 +1,43 @@
-import mongoose from "mongoose";
+import mongoose, { ConnectOptions, Mongoose } from 'mongoose';
 
-const connectDB = async () => {
-    const mongoUri = process.env.MONGO_URI;
-
+const connectDB = async (): Promise<void> => {
+  const mongoUri: string | undefined = process.env.MONGO_URI;
 
   if (!mongoUri) {
     throw new Error('MONGO_URI environment variable not set');
   }
-    try {
-        const conn = await mongoose.connect(mongoUri as string);
-        console.log(`MongoDB Connected: ${conn.connection.host}`);
-    } catch (err) {
-        console.error(err);
-        process.exit(1);
-    }
-}
+
+  try {
+    const conn: Mongoose = await mongoose.connect(mongoUri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    } as ConnectOptions);
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (err) {
+    console.error('MongoDB connection error:', err);
+    process.exit(1);
+  }
+};
 
 export default connectDB;
+
+// import mongoose from "mongoose";
+
+// const connectDB = async () => {
+//     const mongoUri = process.env.MONGO_URI;
+
+
+//   if (!mongoUri) {
+//     throw new Error('MONGO_URI environment variable not set');
+//   }
+//     try {
+//         const conn = await mongoose.connect(mongoUri as string);
+//         console.log(`MongoDB Connected: ${conn.connection.host}`);
+//     } catch (err) {
+//         console.error(err);
+//         process.exit(1);
+//     }
+// }
+
+// export default connectDB;
